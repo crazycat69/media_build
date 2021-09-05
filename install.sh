0d4012e260bfcd2f575d461f78a5e1ad97ccb957
CrazyCat <crazycat69@narod.ru>
2021-09-05 17:19:16 +0300

    Fix install path and extend clean list.

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Disable certain modules that do not compile
-sed -i '/VIDEO_OV9650/d' ./v4l/versions.txt && sed -i '/9.255.255/a VIDEO_OV9650' ./v4l/versions.txt
-sed -i '/VIDEO_OV772X/d' ./v4l/versions.txt && sed -i '/9.255.255/a VIDEO_OV772X' ./v4l/versions.txt
-
 # Enable some staging drivers
 make stagingconfig
+
+# Disable RC/IR support
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
 
@@ -16,9 +14,8 @@ echo "V4L drivers installing..."
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/media
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/staging/media
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/misc/altera-stapl
-sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/linux
-sudo rm -r -f /lib/modules/$(uname -r)/extra
-
+#sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/linux
+sudo rm -r -f /lib/modules/$(uname -r)/updates/extra
 
 sudo make install
 
